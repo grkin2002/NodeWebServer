37d642fd0f6c621d159570bcb99762188372dc52
greg pong <grkin2002@gmail.com>
2018-08-28 22:12:36 +1000

    after eslint

--- server.js
+++ server.js
@@ -6,13 +6,9 @@ const app = express();
 
 hbs.registerPartials(`${__dirname}/views/partials`);
 
-hbs.registerHelper('getCurrentYear', () => {
-  return new Date().getFullYear();
-});
+hbs.registerHelper('getCurrentYear', () => new Date().getFullYear());
 
-hbs.registerHelper('screamIt', (text) => {
-  return text.toUpperCase();
-});
+hbs.registerHelper('screamIt', text => text.toUpperCase());
 
 app.set('view engine', 'hbs');
 
@@ -38,19 +34,19 @@ app.get('/', (req, res) => {
   res.render('home.hbs', {
     pageTitle: 'Home Page',
     currentYear: new Date().getFullYear(),
-    welcomeMessage: 'Welcome to homepage!'
+    welcomeMessage: 'Welcome to homepage!',
   });
 });
 
 app.get('/about', (req, res) => {
   res.render('about.hbs', {
-    pageTitle: 'About Page Dynamic'
+    pageTitle: 'About Page Dynamic',
   });
 });
 
 app.get('/bad', (req, res) => {
   res.send({
-    errorMessage: 'Unable to handle request !'
+    errorMessage: 'Unable to handle request !',
   });
 });
 
